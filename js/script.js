38d37918fd427f28bf8571b5f3a2a01fabe07007
Diana Nguyen <drex.nguyen@gmail.com>
2026-09-10 16:55:13 -0500

    Added button script outline

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const explodeButton = document.getElementById("explode-button");
+
+function explodeChungus(event) {
+    // DO SOMETHING
+}
+
+explodeButton.addEventListener('click', explodeChungus); 
+
